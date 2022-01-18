127324c55fe906d113cfcab5bf8ea77a71f534ff
Pier <491172+plrigaux@users.noreply.github.com>
2022-01-18 15:55:40 -0500

    script build client to server

--- scripts/copyClientFilesToServer.js
+++ scripts/copyClientFilesToServer.js
@@ -1,9 +1,8 @@
-const mkdirp = require('mkdirp')
 const fsExtra = require('fs-extra')
 
 
 
-const source = "./dist/file-server"
+const source = "./client/dist/file-server"
 const dest = "./server/client"
 
 console.log("Copy client's built files to server.")
@@ -11,12 +10,14 @@ console.log(`Copy START`)
 
 console.log("Create directory if not exist")
 // return value is a Promise resolving to the first directory created
-const made = mkdirp.sync(dest)
-if (made) {
-  console.log(`\tdirectory ${made} created.`)
-} else {
-  console.log(`\tdirectory ${dest} exists.`)
+
+try {
+  fsExtra.ensureDirSync(dest)
+  console.log(`\tdirectory ${dest} ensured.`)
+} catch (err) {
+  console.error(err)
 }
+
 
 console.log("Clean directory")
 fsExtra.emptyDirSync(dest)
@@ -24,9 +25,10 @@ fsExtra.emptyDirSync(dest)
 console.log(`Copy files from ${source} to ${dest}`)
 fsExtra.copy(source, dest, {
   overwrite: true
-}, err => {
-  if (err) return console.error(err)
-  console.log('\tsuccess!')
+}).then(() => {
+  console.log('\tcopy success!')
+}).catch(err => {
+  return console.error(err)
+}).then(() => {
+  console.log(`Copy END`)
 })
-
-console.log(`Copy END`)
