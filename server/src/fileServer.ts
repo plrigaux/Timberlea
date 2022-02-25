5a67eaabfabb6649d4a80ddfcd6aabfb266a6a27
plrigaux <491172+plrigaux@users.noreply.github.com>
2022-02-25 13:09:23 -0500

    can download dot file

--- server/src/fileServer.ts
+++ server/src/fileServer.ts
@@ -308,7 +308,19 @@ fileServer.get(endpoints.DOWNLOAD + '/:path', (req: Request, res: Response) => {
     //console.log(`filePath`, filePath, filePathResolved)
 
     if (filePathResolved) {
-        res.download(filePathResolved?.getPathServer());
+
+        let options = { dotfiles : "allow" }
+
+        res.download(filePathResolved.getPathServer(), filePathResolved.getFileName(), options, (err: Error) => {
+            if (err) {
+                console.error("download error", filePath, filePathResolved?.getPathServer(), JSON.stringify(err))
+                if (!res.headersSent) {
+                    res.status(HttpStatusCode.NOT_FOUND).send("File not found: " + filePath) //TODO set an error handler
+                }
+            } else {
+                // decrement a download credit, etc.
+            }
+        });
         return
     }
 
