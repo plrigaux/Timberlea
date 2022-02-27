3663cbfd13cfc6168b86397a77faaf4d9889a115
plrigaux <491172+plrigaux@users.noreply.github.com>
2022-02-26 23:38:34 -0500

    test report

--- server/jest.config.js
+++ server/jest.config.js
@@ -2,5 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-//  silent : true
+  reporters: [
+    "default",
+    ["./node_modules/jest-html-reporter", {
+      "pageTitle": "Test Report"
+    }]
+  ]
 };
