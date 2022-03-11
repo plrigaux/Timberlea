d3ed0bb0390813968d83c92c98d70f792e612296
plrigaux <491172+plrigaux@users.noreply.github.com>
2022-03-11 10:05:00 -0500

    pre Licence

--- scripts/copyClientFilesToServer.js
+++ scripts/copyClientFilesToServer.js
@@ -1,32 +1,32 @@
-const fsExtra = require('fs-extra')
-
-const source = "./client/dist/file-server"
+import fsExtra from 'fs-extra'
+import chalk from 'chalk';
+const source = "./client/dist/timberlea-client"
 const dest = "./server/client"
 
-console.log("Copy client's built files to server.")
-console.log(`Copy START`)
+console.log(chalk.green("Copy client's built files to server."))
+console.log(chalk.green(`Copy START`))
 
-console.log("Create directory if not exist")
+console.log(chalk.yellowBright("Create directory if not exist"))
 // return value is a Promise resolving to the first directory created
 
 try {
   fsExtra.ensureDirSync(dest)
-  console.log(`\tdirectory ${dest} ensured.`)
+  console.log(chalk.green(`\tdirectory ${dest} ensured.`))
 } catch (err) {
   console.error(err)
 }
 
 
-console.log("Clean directory")
+console.log(chalk.blue("Clean directory"))
 fsExtra.emptyDirSync(dest)
 
-console.log(`Copy files from ${source} to ${dest}`)
+console.log(chalk.blue(`Copy files from ${source} to ${dest}`))
 fsExtra.copy(source, dest, {
   overwrite: true
 }).then(() => {
-  console.log('\tcopy success!')
+  console.log(chalk.green('\tCopy success!'))
 }).catch(err => {
   return console.error(err)
 }).then(() => {
-  console.log(`Copy END`)
+  console.log(chalk.blue(`Copy END`))
 })
