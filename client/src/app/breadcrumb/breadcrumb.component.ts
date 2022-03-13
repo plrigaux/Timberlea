3d961e52ccdd040400c5d124d5c10742ff996e9a
plrigaux <491172+plrigaux@users.noreply.github.com>
2022-03-13 00:01:42 -0500

    breadcrumb backing

--- client/src/app/breadcrumb/breadcrumb.component.ts
+++ client/src/app/breadcrumb/breadcrumb.component.ts
@@ -9,48 +9,20 @@ import { FileServerService } from '../utils/file-server.service';
 export class BreadcrumbComponent implements OnInit {
   private remoteDirectory = ""
   pathChip: PathChip[] = []
-  private re = /\//
+  private re = "/"
 
   constructor(private fileServerService: FileServerService) { }
 
   ngOnInit(): void {
     this.fileServerService.subscribeRemoteDirectory({
       next: (remoteDirectory: string) => {
-        this.remoteDirectory = remoteDirectory
-        this.pathChip = []
-
-        let pc: PathChip = {
-          name: "🏠",
-          path: ""
-        }
-        this.pathChip.push(pc)
-
-        if (remoteDirectory === "") {
-          return
-        }
-        
-        let path = ""
-        let splittedRemoteDir = remoteDirectory.split(this.re)
-        //console.log("splittedRemoteDir", remoteDirectory, splittedRemoteDir)
-
-        splittedRemoteDir.forEach(s => {
-          if (path.length > 0) {
-            path += "/" + s
-          } else {
-            path = s
-          }
-
-          let pc: PathChip 
-            pc = {
-              name: s,
-              path: path
-            }
-
-          this.pathChip.push(pc)
-        })
-
+        this.setUpChips(remoteDirectory);
       }
+
     })
+
+    //init empty
+    this.setUpChips("");
   }
 
   clickChip(chip: PathChip) {
@@ -58,9 +30,73 @@ export class BreadcrumbComponent implements OnInit {
 
     this.fileServerService.list(chip.path)
   }
+
+  private setUpChips(remoteDirectory: string) {
+    this.remoteDirectory = remoteDirectory;
+
+    let newPathChip: PathChip[] = [];
+    //this.pathChip = []
+    let pcRoot: PathChip = {
+      name: "🏠",
+      path: "",
+      selected: false
+    };
+
+    newPathChip.push(pcRoot);
+
+    if (remoteDirectory !== "") {
+      let path = "";
+      let splittedRemoteDir = remoteDirectory.split(this.re);
+
+      //set the path for each chips
+      splittedRemoteDir.forEach((dirName) => {
+        if (path.length > 0) {
+          path += "/" + dirName;
+        } else {
+          path = dirName;
+        }
+
+        let pathChip: PathChip;
+        pathChip = {
+          name: dirName,
+          path: path,
+          selected: false
+        };
+
+        newPathChip.push(pathChip);
+      });
+
+      if (newPathChip.length >= 0) {
+        newPathChip[newPathChip.length - 1].selected = true;
+      }
+    } else {
+      pcRoot.selected = true;
+    }
+
+    let newLength = newPathChip.length;
+    let oldLength = this.pathChip.length;
+
+    console.log("new", newLength, newPathChip);
+    console.log("old", oldLength, this.pathChip);
+
+    if (newLength < oldLength) {
+      let oldNextPath = this.pathChip[newLength].path;
+      let lastNewPath = newPathChip[newLength - 1].path;
+
+      if (oldNextPath.startsWith(lastNewPath)) {
+        for (let i = newLength; i < oldLength; i++) {
+          this.pathChip[i].selected = false;
+          newPathChip.push(this.pathChip[i]);
+        }
+      }
+    }
+
+    this.pathChip = newPathChip;
+  }
 }
 
 interface PathChip {
   name: string;
-  path: string
+  path: string;
+  selected: boolean
 }
